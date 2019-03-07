3f9dd872185893a5f620224d8ab334877968c413
johnnyvaca <Johnny.VACA-JARAMILLO@cpnv.ch>
2019-03-07 16:58:10 +0100

    wip affichage de l'aide
    touches getchar

--- development/bataille-navaleDev/main.c
+++ development/bataille-navaleDev/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <conio.h>
+void afficher_aide()
+        {
+            int touche;
+            do {
+                touche = _getch();
 
-int main() {
-    printf("Hello, World!\n");
+                printf("%d\n", touche);      // print numeric key value
+            } while (touche != 59);          // end when Esc pressed
+        }
+int main()
+{
+    afficher_aide();
+
+
     return 0;
 }
