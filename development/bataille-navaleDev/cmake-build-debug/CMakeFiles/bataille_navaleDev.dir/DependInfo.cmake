b3f41047233dd2cbf704880b84f95587c3bf2560
johnnyvaca <47849728+johnnyvaca@users.noreply.github.com>
2019-03-07 23:51:03 +0100

    Ajout du bouton F1

--- development/bataille-navaleDev/cmake-build-debug/CMakeFiles/bataille_navaleDev.dir/DependInfo.cmake
+++ development/bataille-navaleDev/cmake-build-debug/CMakeFiles/bataille_navaleDev.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Johnny.VACA-JARAMILL/Documents/projets/bataille-navale/development/bataille-navaleDev/main.c" "C:/Users/Johnny.VACA-JARAMILL/Documents/projets/bataille-navale/development/bataille-navaleDev/cmake-build-debug/CMakeFiles/bataille_navaleDev.dir/main.c.obj"
+  "E:/Documents/Projets/BN-JVA-Bataille-Navale/development/bataille-navaleDev/main.c" "E:/Documents/Projets/BN-JVA-Bataille-Navale/development/bataille-navaleDev/cmake-build-debug/CMakeFiles/bataille_navaleDev.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- development/bataille-navaleDev/main.c
+++ development/bataille-navaleDev/main.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <conio.h>
+#include <windows.h>
 void afficher_aide()
         {
-            int touche;
-            do {
-                touche = _getch();
+      int ch;
+            do {system("cls");
+                printf("AIDE");
+                ch = _getch();
 
-                printf("%d\n", touche);      // print numeric key value
-            } while (touche != 59);          // end when Esc pressed
+            } while (ch != 27);
+
         }
 int main()
-{
-    afficher_aide();
+{      int touche;
+    do {
+        printf("MENU\n\n");
+        printf("ENTER : JOUER     ||      F1 : l'aide     ||     ESCAPE :  quiter \n\n");
+        touche = _getch();
+        switch (touche)
+        {
+            case 109:
+
+                break;
+            case 59 :
+                system("cls");
+                afficher_aide();
+                break;
+            case 13:
+                system("cls");
+                printf("jouer");
+                break;
+            case 27:
+                return 0;
+            default: main();
+        }
+    } while (touche != 27);
+
 
 
     return 0;
