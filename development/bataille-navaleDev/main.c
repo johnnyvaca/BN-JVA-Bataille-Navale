c5c06f7326da839d06a206f220a6682fb48230cb
johnnyvaca <Johnny.VACA-JARAMILLO@cpnv.ch>
2019-03-14 16:55:24 +0100

    wip affichage grille

--- development/bataille-navaleDev/main.c
+++ development/bataille-navaleDev/main.c
@@ -2,6 +2,32 @@
 #include <conio.h>
 #include <windows.h>
 
+int position = 0;
+void raccourcis(){
+
+#define STLC 218 // ┌, Single Top Left Corner
+#define STRC 191 // ┐, Single Top Right Corner
+#define SBLC 192 // └, Single Bottom Left Corner
+#define SBRC 217 // ┘, Single Bottom Right Corner
+#define SVSB 179 // │, Single Vertical Simple Border
+#define SVRB 180 // ┤, Single Vertical Right Border
+#define SVLB 195 // ├, Single Vertical Left Border
+#define SHSB 196 // ─, Single Horizontal Simple Border
+#define SHBB 193 // ┴, Single Horizontal Bottom Border
+#define SHTB 194 // ┬, Single Horizontal Top Border
+#define SC   197 // ┼, Single Center
+#define DTLC 201 // ╔, Double Top Left Corner
+#define DTRC 187 // ╗, Double Top Right Corner
+#define DBLC 200 // ╚, Double Bottom Left Corner
+#define DBRC 188 // ╝, Double Bottom Right Corner
+#define DVSB 186 // ║, Double Vertical Simple Border
+#define DVRB 185 // ╣, Double Vertical Right Border
+#define DVLB 204 // ╠, Double Vertical Left Border
+#define DHSB 205 // ═, Double Horizontal Simple Border
+#define DHBB 202 // ╩, Double Horizontal Bottom Border
+#define DHTB 203 // ╦, Double Horizontal Top Border
+#define DC   206 // ╬, Double Center
+};
 void logo() {
     printf("              ____        _        _ _ _        _    _                  _      \n"
            "             |  _ \\     | |      (_) | |      | \\ | |                | |     \n"
@@ -32,33 +58,69 @@ void menu() {
     printf("F1 AIDE");
     printf("\n");
 }
+void affichageGrille() {
+    raccourcis();
+    int ch;
+#pragma execution_caracter_set("UTF-8")
+    SetConsoleOutputCP(65001); // For accented characters
+    SetConsoleOutputCP(437); // For semi-graphic characters
+    do {
+            system("cls");
 
-void grilles() {
+        printf("%c%c%c%c%c\n",STLC,SHSB,SHSB,SHSB,STRC);
+        printf("%c   %c\n",SVSB,SVSB,SVSB);
+        printf("%c%c%c%c%c\n",SBLC,SHSB,SHSB,SHSB,SBRC);
+
+/*
+
+        printf ("%c%c%c%c%c%c%c%c%c\n",DTLC,DHSB,DHSB,DHSB,DHTB,DHSB,DHSB,DHSB,DTRC);
+        printf ("%c   %c   %c\n",DVSB,DVSB,DVSB);
+        printf ("%c%c%c%c%c%c%c%c%c\n",DVLB,DHSB,DHSB,DHSB,DC,DHSB,DHSB,DHSB,DVRB);
+        printf ("%c   %c   %c\n",DVSB,DVSB,DVSB);
+        printf ("%c%c%c%c%c%c%c%c%c\n",DBLC,DHSB,DHSB,DHSB,DHBB,DHSB,DHSB,DHSB,DBRC); */
+
+
+     //printf("\n\n\n%d",ch);
+        ch = _getch();
+    } while (ch != 27);
+system("cls");
+
+}
+void menuGrilles() {
     int ch = -1;
-    logo();
+
 
     do {
         system("cls");
+
+        logo();
         printf("                              CHOISISSEZ VOTRE MER...");
         printf("\n\n\n");
-        printf("                                   1. MER FIXE");
-        printf("\n\n\n\n\n");
-        if(ch != 49 && ch != -1)
-        {
+        printf("                                   1. MER FIXE\n");
+        printf("                                   2. AFFICHAGE GRILLE");
+        printf("\n\n\n\n");
+        if (ch != 49 && ch != -1 ) {
             printf("                                                           ");
-            printf("CETTE MER N'EXI\fSTE PAS !!!");
+            printf("CETTE MER N'EXISTE PAS !!!");
+        }
+        if (ch == 49) {
+system("cls");
+printf("\n\ngrille fixe");
+        }
+        if(ch == 50)
+        {
+            system("cls");
+            position =  49;
+            affichageGrille();
         }
         printf("\n\n\n\n");
         printf("APPUYER SUR LE NUMERO CORRESPONDANT POUR CONTINUER :   ");
-//printf("%d", ch);
         ch = _getch();
+
     } while (ch != 27);
 }
 
-void grilleFIxe()
-{
 
-}
 
 void afficher_aide() {
     int ch;
@@ -83,12 +145,13 @@ void afficher_aide() {
         ch = _getch();
 
     } while (ch != 27);
-
 }
 
 int main() {
+
 #pragma execution_caracter_set("UTF-8")
     SetConsoleOutputCP(CP_UTF8); // For accented characters
+
     int ch;
     system("color f1");
     int touche;
@@ -101,10 +164,12 @@ int main() {
             case 59 :
                 system("cls");
                 afficher_aide();
+                position = 59;
                 break;
             case 13:
                 system("cls");
-                grilles();
+                menuGrilles();
+                position = 13;
                 break;
             case 27:
                 break;
@@ -113,6 +178,8 @@ int main() {
             default:
                 main();
         }
+
     } while (touche != 27);
     system("cls");
+  //  system("C:\\Users\\Johnny.VACA-JARAMILL\\Documents\\projets\\BN-JVA-Bataille-Navale\\development\\bataille-navaleDev\\cmake-build-debug\\bataille_navaleDev.exe");
 }
